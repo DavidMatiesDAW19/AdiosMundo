67c7093fac9bfb58a5d7f7d4eb9c7b57fd97e7b7
David Maties <dmaties15@gmail.com>
2019-10-03 18:26:33 +0200

    Experimentando con colores2

--- adiosmundo.java
+++ adiosmundo.java
@@ -2,8 +2,9 @@ public class adiosmundo{
 	public static void main (String[] args){
 	String amarillo="\033[33m";
 	String verde="\033[32m";
-		System.out.println(amarillo+"Adios Mundo!");
-		System.out.println(verde+"Pero... volveré!");
+	String reset="\033[0m";
+		System.out.println(amarillo+"Adios Mundo!"+reset);
+		System.out.println(verde+"Pero... volveré!"+reset);
 }
 
 }
